4c22300024a6eddc10b91e722fdd4a173baa7c9d
Chizuui <desckun@gmail.com>
2026-04-03 05:14:40 +0700

    feat: create home page view with jumbotron layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,6 @@
         <div class="container">
             <h1 class="display-4">Home Page</h1>
             <p class="lead">Selamat Datang</p>
-
-            <p><strong>Nama :</strong> {{ $nama }}</p>
-
-            <p><strong>Mata Kuliah</strong></p>
-            <ul>
-                @foreach($matkul as $p)
-                    <li>{{ $p }}</li>
-                @endforeach
-            </ul>
         </div>
     </div>
 @endsection
