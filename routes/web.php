5c92b14f9fd375dcc9a8ad19cf1070f319c501b3
Chizuui <desckun@gmail.com>
2026-04-02 08:19:59 +0700

    feat: initialize frontend layout, assets, and home controller for project structure

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManagementUserController;
+use App\Http\Controllers\Frontend\HomeController;
+
+// Route untuk template Butterfly
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/user', ManagementUserController::class);
-
-
-Route::get('/home', function () {
-    return view('home');
-});
 
 
